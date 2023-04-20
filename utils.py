857494549e87d37e03682c4e8aa69ad0244c44c1
Subodh Verma <subodh@medvolt.ai>
2023-04-20 23:37:27 +0530

    add utils.py

---
+++ utils.py
@@ -0,0 +1,13 @@
+def format_name(name):
+    name = name.lower().strip().replace(" ", "")
+    name = ''.join(e for e in name if e.isalnum())
+    return name
+
+async def fetch(url, session):
+    async with session.get(url) as response:
+        return await response.json()
+
+def add_to_file(file_name, data):
+    with open(file_name, 'a') as f:
+        f.write(data + '\n')
+        
