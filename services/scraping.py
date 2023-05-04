611e549665761ef3a0ba55198a93699741753eea
You Name <subodh@medvolt.ai>
2023-05-05 05:23:11 +0530

    Committing new untested changed, especially scroll

--- services/scraping.py
+++ services/scraping.py
@@ -1,5 +1,5 @@
 # import web driver
-import os, sys, time, csv, asyncio
+import os, time, csv, random
 
 from bs4 import BeautifulSoup
 from selenium import webdriver
@@ -10,6 +10,15 @@ from services.email_verify import EmailVerify
 
 field_names = ['Name', 'Title', 'Company', 'Link']
 
+def get_user_agent():
+    user_agents = [
+        # "Mozilla/5.0 (Linux; Android 6.0.1; SM-G532G Build/MMB29T) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Mobile Safari/537.36",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36", 
+        # "Mozilla/5.0 (Linux; Android 6.0.1; SM-G532G Build/MMB29T) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.4389.114 Mobile Safari/537.36",
+        # "Mozilla/5.0 (Linux; Android 6.0.1; SM-G532G Build/MMB29T) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.4389.114 Mobile Safari/537.36",
+    ]
+    return user_agents[random.randint(0, len(user_agents) - 1)]
 
 def write_to_csv(data):
     with open('output.csv', 'a', newline='', encoding="utf-8") as csvfile:
@@ -18,7 +27,7 @@ def write_to_csv(data):
 
 
 class ScrapingService:
-    sales_url = "https://www.linkedin.com/sales/login"
+    login_url = "https://www.linkedin.com/sales/login"
 
     def __init__(self, url, start_page, end_page, extraction_id):
         self.url = url
@@ -26,7 +35,7 @@ class ScrapingService:
         self.current_page = start_page
         self.end_page = end_page
         self.extraction_id = extraction_id
-        self.speed = 5  # lower is faster
+        self.speed = 1  # lower is faster
         self.total_scroll_time = 25  # seconds
         self.get_creds()
         self.init_driver()
@@ -47,9 +56,12 @@ class ScrapingService:
         options.add_argument("--disable-setuid-sandbox")
         options.add_argument("--disable-logging")
         options.add_argument("--log-level=3")
-        self.browser = webdriver.Chrome(
-            '/usr/local/bin/chromedriver', options=options)
-        self.browser.get(self.sales_url)
+        browser = webdriver.Chrome('/usr/local/bin/chromedriver', options=options)
+        browser.execute_cdp_cmd("Network.setUserAgentOverride", {
+            "userAgent": get_user_agent(),
+        })
+        self.browser = browser
+        self.browser.get(self.login_url)
         print("Successfully initiated Web Driver")
 
     def login(self):
@@ -63,37 +75,26 @@ class ScrapingService:
             'xpath', '//*[@type="submit"]')
         log_in_button.click()
         print("Successfully logged in")
+        time.sleep(1000)
 
-    def wait(self, wait_time):
-        print(f"Waiting for {wait_time} seconds")
-        asyncio.sleep(wait_time)
+    def wait(self, wait_time, wait_message=""):
+        print(f"Waiting for {wait_time} seconds : {wait_message}")
+        time.sleep(wait_time)
+        # asyncio.sleep(wait_time)
         # for i in range(wait_time):
         #     print(f"Waiting for {wait_time} seconds" + "." * i)
         #     sys.stdout.write("\033[F")
-        #     asyncio.sleep(1)
+        #     time.sleep(1)
 
-    def scroll_till_end(self):
-        error_count = 0
-        self.wait(3*self.speed)
-        start = time.time()
-        initialScroll = 0
-        finalScroll = 500
-        while True:
-            self.wait(1*self.speed)
-            try:
-                self.browser.execute_script(
-                    f"document.getElementById('search-results-container').scroll({initialScroll},{finalScroll})")
-                initialScroll = finalScroll
-                finalScroll += 500
-                self.wait(2*self.speed)
-                end = time.time()
-                if round(end - start) > self.total_scroll_time:
-                    break
-            except Exception as e:
-                print("Error in scrolling down, continuing...", str(e))
-                error_count += 1
-                if error_count > 2:
-                    break
+    def scroll_results_container(self):        
+        scroll_step = 200
+        scroll_height = 0
+        for _ in range(self.total_scroll_time):
+            self.wait(1, "Scrolling")
+            self.browser.execute_script(f"document.getElementById('search-results-container').scroll(0,{scroll_height})")
+            scroll_height += scroll_step
+        
+            
 
     def next_page(self):
         self.wait(1*self.speed)
@@ -111,7 +112,7 @@ class ScrapingService:
         self.wait(1*self.speed)
 
     def get_page_data(self):
-        self.scroll_till_end()
+        self.scroll_results_container()
 
         extracted_data = []
         src = self.browser.page_source
@@ -151,14 +152,26 @@ class ScrapingService:
             return False
 
     def start_scraping(self):
-        total_pages = self.end_page - self.start_page + 1
-
         self.url = self.url + "&page=" + str(self.start_page)
         self.browser.get(self.url)
+        self.wait(3*self.speed, f"Waiting for page {self.start_page} to load")
 
-        self.wait(2*self.speed)
+        # search for document.getElementById('search-results-container') element for 10 seconds
+        # if not found, then wait for human check
+        # if found, then scroll till end
+        timeout = time.time() + 20
+        for i in range(10):
+            try:
+                self.browser.find_element(By.ID, "search-results-container")
+                break
+            except Exception as e:
+                self.wait(2, "Waiting for search-results-container element")
+                if time.time() > timeout:
+                    print("Error in getting page data: ", str(e))
+                    human_check_wait = input("Error in getting page data, please solve the captcha and press enter to continue:")
+                    print("human_check_wait: ", human_check_wait)
 
-        for _ in range(total_pages):
+        while self.current_page <= self.end_page:
             run_page_data = self.get_page_data()
             if run_page_data:
                 self.next_page()
@@ -167,6 +180,7 @@ class ScrapingService:
 
     def process_extracted_data(self, extracted_data):
         print("\nProcessing extracted data")
+        # print("Extracted data: ", extracted_data)
         email_verify = EmailVerify(
             linkedin_data=extracted_data, extraction_id=self.extraction_id)
         email_verify.process_data()
